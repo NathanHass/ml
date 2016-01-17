8142c7a73766eeeb4f772e1e7ca4180a5643273f
Nathan Hass <nathan@upstatement.com>
2016-01-17 18:38:38 -0500

    added gruntfile to compile scss

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var autoprefixer = require('gulp-autoprefixer');
+var $ = require('gulp-load-plugins')();
+
+// define tasks here
+
+
+gulp.task('sass', function () {
+  return gulp.src('static/scss/*.scss')
+
+    // Compile Sass
+    .pipe(
+      $.sass({
+        style: 'nested',
+      })
+      .on('error', $.sass.logError)
+    )
+
+    // Run CSS through autoprefixer
+    .pipe($.autoprefixer('last 10 version'))
+
+    // Write development assets
+    .pipe(gulp.dest('static/css'))
+
+});
+
+gulp.task('default', ['sass']);
+
+gulp.task('watch', function () {
+  gulp.watch('static/scss/*/**.scss', ['sass']);
+});
